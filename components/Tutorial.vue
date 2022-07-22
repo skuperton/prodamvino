b2d94cac29619fab3cfcb7799bbbc9c9e76c547a
inflex <inflex_off@vk.com>
2022-07-22 13:59:04 +0300

    init nuxt ssr application

---
+++ components/Tutorial.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  section.b-main
+    .__header
+      h2.__title For example
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Tutorial extends Vue {
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  tutorial-component
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import Tutorial from '~/components/Tutorial.vue'
+
+@Component({
+  components: {
+    'tutorial-component': Tutorial
+  }
+})
+export default class Home extends Vue {
+}
+</script>
