7323533007207fe0d20b23428aaa3bce8daddf7d
inflex <inflex_off@vk.com>
2022-08-24 20:24:00 +0300

    fix hero section size

--- components/blanks/social.vue
+++ components/blanks/social.vue
@@ -5,6 +5,8 @@
       :key="social.id"
       :href="social.url"
       :style="{'background-color': social.color}"
+      target="_blank"
+      rel="noopener noreferrer",
     )
       font-awesome-icon.__icon(
         :icon="[social.prefix, social.iconName]"
@@ -26,21 +28,21 @@ export default class Social extends Vue {
       id: 1,
       prefix: 'fab',
       iconName: 'telegram',
-      url: '#',
+      url: 'https://t.me/+79362734449',
       color: '#0088CC'
     },
     {
       id: 2,
       prefix: 'fab',
       iconName: 'whatsapp',
-      url: '#',
+      url: 'https://wa.me/79362734449',
       color: '#25D366'
     },
     {
       id: 3,
       prefix: 'fab',
       iconName: 'viber',
-      url: '#',
+      url: 'https://skobelkin.ru/viber/79362734449',
       color: '#7F40BD'
     },
     {
