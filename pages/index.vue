323f20bbe488ff716a10d128f99ebddc23900644
br4instormer <br4instormer@gmail.com>
2023-07-24 18:31:41 +0300

    Add meta for index

--- pages/index.vue
+++ pages/index.vue
@@ -31,8 +31,20 @@ import Redemption from '~/components/sections/redemption.vue'
     'popularity-component': Popularity,
     'products-component': Products,
     'hero-component': Hero
+  },
+  head () {
+    return {
+      title: 'Скупка алкоголя. Он-лайн калькулятор выкупа алкоголя',
+      meta: [
+        {
+          name: 'description',
+          content: 'Продать элитный или коллекционный алкоголь с выгодной оценкой. Наши курьеры в Москве проведут сделку быстро ,на лучших условиях'
+        }
+      ]
+    }
   }
 })
 export default class Home extends Vue {
+
 }
 </script>
